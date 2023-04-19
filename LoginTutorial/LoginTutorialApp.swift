3cf42a6fb45617dbba8c28c5a7cc73284a7bbd12
dayjack <frostjanna12@gmail.com>
2023-04-19 19:24:22 +0900

    Initial Commit

---
+++ LoginTutorial/LoginTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginTutorialApp.swift
+//  LoginTutorial
+//
+//  Created by ChoiYujin on 2023/04/19.
+//
+
+import SwiftUI
+
+@main
+struct LoginTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
